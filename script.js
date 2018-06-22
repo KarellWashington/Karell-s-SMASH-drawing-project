7c039a094f4b17efe39b4d429a4cba9936d294a2
Karell <karell.washington@scholar.smashprogram.org>
2018-06-22 10:03:27 -0700

    commit two

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function setup() {
   p5Canvas = createCanvas(800, 600);
   p5Canvas.parent("#p5-canvas");
   myName = select("#my-name");
-  myName.html("[Your Name Here]"); // enter your name here!
+  myName.html("Karell"); // enter your name here!
+  noLoop();
 }
 
  /*
@@ -22,9 +23,9 @@ function setup() {
 // Write all your code inside the draw() function below!
 function draw() {
   // Example. You can remove when you're ready!
-  background(0, 255, 0); // try changing this to a blue background
+  background(25, 50, 255); // try changing this to a blue background
   fill(255, 204, 0);    // Always call fill() before the shape you want to fill in.
-  rect(30, 20, 55, 55); // Draw a 30x20 pixel rectangle at coordinate (55,55)
+  rect(130, 120, 155, 100); // Draw a 30x20 pixel rectangle at coordinate (55,55)
   triangle(30,75,58,20,86,75);
   line(30, 20, 85, 20);
 stroke(126);
@@ -32,4 +33,6 @@ line(85, 20, 85, 75);
 stroke(255);
 line(85, 75, 30, 75);
 line(30, 20, 85, 75);
+quad(38, 31, 86, 20, 69, 63, 30, 76);
+console.log("I am below line function");
 }
